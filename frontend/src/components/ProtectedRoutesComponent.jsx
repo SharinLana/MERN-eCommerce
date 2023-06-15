ffc40a8970be7cba95172b5dae7d4ffc3530f694
Lana Sharin <lana.sharin.webdev@gmail.com>
2023-06-15 13:03:14 -0700

    change the component name

--- frontend/src/components/ProtectedRoutesComponent.jsx
+++ frontend/src/components/ProtectedRoutesComponent.jsx
@@ -1,9 +1,9 @@
 import React from "react";
 import { Outlet, Navigate } from "react-router-dom";
 
-const ProtectedRouteComponent = () => {
+const ProtectedRoutesComponent = () => {
   let auth = true;
   return auth ? <Outlet /> : <Navigate to="/login" />;
 };
 
-export default ProtectedRouteComponent;
+export default ProtectedRoutesComponent;
