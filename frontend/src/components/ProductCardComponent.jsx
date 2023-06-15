7a9413c8ead78bf17623b8a37ae79603e4ea73e3
Lana Sharin <lana.sharin.webdev@gmail.com>
2023-06-15 16:51:02 -0700

    create a product card component using Bootstrap

---
+++ frontend/src/components/ProductCardComponent.jsx
@@ -0,0 +1,20 @@
+import React from "react";
+import { Button, Card } from "react-bootstrap";
+
+const ProductCardComponent = () => {
+  return (
+    <Card style={{ width: "18rem" }}>
+      <Card.Img variant="top" src="images/games-category.png" />
+      <Card.Body>
+        <Card.Title>Card Title</Card.Title>
+        <Card.Text>
+          Some quick example text to build on the card title and make up the
+          bulk of the card content.
+        </Card.Text>
+        <Button variant="primary">Go somewhere</Button>
+      </Card.Body>
+    </Card>
+  );
+};
+
+export default ProductCardComponent;
