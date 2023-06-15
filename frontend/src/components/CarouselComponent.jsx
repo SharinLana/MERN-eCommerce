9aa4fc9da569d8f15aaf9a8e6ab0b226b9a2c9d4
Lana Sharin <lana.sharin.webdev@gmail.com>
2023-06-15 16:38:20 -0700

    replace the bootstrap default images with the custom pictures

--- frontend/src/components/CarouselComponent.jsx
+++ frontend/src/components/CarouselComponent.jsx
@@ -7,7 +7,7 @@ const CarouselComponent = () => {
       <Carousel.Item>
         <img
           className="d-block w-100"
-          src="holder.js/800x400?text=First slide&bg=373940"
+          src="images/carousel/carousel-1.png"
           alt="First slide"
         />
         <Carousel.Caption>
@@ -18,7 +18,7 @@ const CarouselComponent = () => {
       <Carousel.Item>
         <img
           className="d-block w-100"
-          src="holder.js/800x400?text=Second slide&bg=282c34"
+          src="images/carousel/carousel-2.png"
           alt="Second slide"
         />
 
@@ -30,15 +30,13 @@ const CarouselComponent = () => {
       <Carousel.Item>
         <img
           className="d-block w-100"
-          src="holder.js/800x400?text=Third slide&bg=20232a"
+          src="images/carousel/carousel-3.png"
           alt="Third slide"
         />
 
         <Carousel.Caption>
           <h3>Third slide label</h3>
-          <p>
-            Praesent commodo cursus magna, vel scelerisque nisl consectetur.
-          </p>
+          <p>Present commodo cursus magna, vel scelerisque nisl consectetur.</p>
         </Carousel.Caption>
       </Carousel.Item>
     </Carousel>
