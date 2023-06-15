f7aa7f01c684e4c727af6b813a5fba305efee46a
Lana Sharin <lana.sharin.webdev@gmail.com>
2023-06-15 16:55:16 -0700

    render the CategoryCardComponent inside of the HopePage

--- frontend/src/pages/HomePage.jsx
+++ frontend/src/pages/HomePage.jsx
@@ -1,10 +1,12 @@
 import React from "react";
 import CarouselComponent from "../components/CarouselComponent";
+import CategoryCardComponent from "../components/CategoryCardComponent";
 
 const HomePage = () => {
   return (
     <div>
       <CarouselComponent />
+      <CategoryCardComponent />
     </div>
   );
 };
