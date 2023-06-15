e3a384f3b79abbd60225c5d2d6115f460a6d6ea8
Lana Sharin <lana.sharin.webdev@gmail.com>
2023-06-15 13:41:32 -0700

    render admin routes in App.jsx and passed a static value to the ProtectedRoutesComponent

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -5,11 +5,22 @@ import LoginPage from "./pages/LoginPage";
 import RegisterPage from "./pages/RegisterPage";
 import ProductListPage from "./pages/ProductListPage";
 import ProductDetailsPage from "./pages/ProductDetailsPage";
+// User Pages
 import UserProfilePage from "./pages/user/UserProfilePage";
 import UserOrderPage from "./pages/user/UserOrderPage";
 import UserOrderDetailsPage from "./pages/user/UserOrderDetailsPage";
 import UserCartDetailsPage from "./pages/user/UserCartDetailsPage";
 import ProtectedRoutesComponent from "./components/ProtectedRoutesComponent";
+// Admin pages
+import AdminUsersPage from "./pages/admin/AdminUsersPage";
+import AdminProductsPage from "./pages/admin/AdminProductsPage";
+import AdminOrdersPage from "./pages/admin/AdminOrdersPage";
+import AdminOrderDetailsPage from "./pages/admin/AdminOrderDetailsPage";
+import AdminEditUserPage from "./pages/admin/AdminEditUserPage";
+import AdminEditProductPage from "./pages/admin/AdminEditProductPage";
+import AdminCreateProductPage from "./pages/admin/AdminCreateProductPage";
+import AdminChatsPage from "./pages/admin/AdminChatsPage";
+import AdminAnalyticsPage from "./pages/admin/AdminAnalyticsPage";
 
 function App() {
   return (
@@ -23,7 +34,7 @@ function App() {
         <Route path="/cart" element={<CartPage />} />
         <Route path="*" element="Page not found 404" />
 
-        {/* Protected routes */}
+        {/* User Protected Routes */}
         <Route element={<ProtectedRoutesComponent />}>
           <Route path="/user" element={<UserProfilePage />} />
           <Route path="/user/my-orders" element={<UserOrderPage />} />
@@ -32,6 +43,32 @@ function App() {
             element={<UserOrderDetailsPage />}
           />
           <Route path="/user/cart-details" element={<UserCartDetailsPage />} />
+        </Route>
+
+        {/* Admin Protected Routes */}
+        <Route element={<ProtectedRoutesComponent admin={true} />}>
+          <Route path="/admin/users" element={<AdminUsersPage />} />
+          <Route path="/admin/edit-user" element={<AdminEditUserPage />} />
+          <Route path="/admin/products" element={<AdminProductsPage />} />
+          <Route
+            path="/admin/create-new-product"
+            element={<AdminCreateProductPage />}
+          />
+          <Route
+            path="/admin/create-new-product"
+            element={<AdminCreateProductPage />}
+          />
+          <Route
+            path="/admin/edit-product"
+            element={<AdminEditProductPage />}
+          />
+          <Route path="/admin/orders" element={<AdminOrdersPage />} />
+          <Route
+            path="/admin/order-details"
+            element={<AdminOrderDetailsPage />}
+          />
+          <Route path="/admin/chats" element={<AdminChatsPage />} />
+          <Route path="/admin/analytics" element={<AdminAnalyticsPage />} />
         </Route>
       </Routes>
     </BrowserRouter>
