d947f08f250707f9d9008ca68862a952d36a1269
Lana Sharin <lana.sharin.webdev@gmail.com>
2023-06-17 12:35:56 -0700

    displayed a name of each product category in the CategoryCardComponent

--- frontend/src/components/CategoryCardComponent.jsx
+++ frontend/src/components/CategoryCardComponent.jsx
@@ -1,12 +1,12 @@
 import React from "react";
 import { Button, Card } from "react-bootstrap";
 
-const CategoryCardComponent = () => {
+const CategoryCardComponent = ({item}) => {
   return (
     <Card >
       <Card.Img variant="top" src="images/games-category.png" />
       <Card.Body>
-        <Card.Title>Card Title</Card.Title>
+        <Card.Title>{item}</Card.Title>
         <Card.Text>
           Some quick example text to build on the card title and make up the
           bulk of the card content.
