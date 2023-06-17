50ad91f1eb7efb941345db175859ee758d8993e4
Lana Sharin <lana.sharin.webdev@gmail.com>
2023-06-17 12:28:17 -0700

    crate an array of dummy product categories and render each of them in The HomePage component

--- frontend/src/pages/HomePage.jsx
+++ frontend/src/pages/HomePage.jsx
@@ -1,13 +1,30 @@
 import React from "react";
+import { Row, Container } from "react-bootstrap";
 import CarouselComponent from "../components/CarouselComponent";
 import CategoryCardComponent from "../components/CategoryCardComponent";
 
+const categories = [
+  "Audio",
+  "Movies",
+  "Books",
+  "Electronics",
+  "Cars",
+  "Smartphones",
+];
+
 const HomePage = () => {
   return (
-    <div>
+    <>
       <CarouselComponent />
-      <CategoryCardComponent />
-    </div>
+
+      <Container>
+        <Row xs={1} md={2} className="g-4" mt={5}>
+          {categories.map((item, index) => (
+            <CategoryCardComponent item={item} key={index} />
+          ))}
+        </Row>
+      </Container>
+    </>
   );
 };
 
