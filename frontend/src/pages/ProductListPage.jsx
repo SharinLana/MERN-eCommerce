af72395dae81e1f1c253eec2a7474a1e9f60632f
Lana Sharin <lana.sharin.webdev@gmail.com>
2023-06-17 15:32:30 -0700

    start organizing the ProductListPage

--- frontend/src/pages/ProductListPage.jsx
+++ frontend/src/pages/ProductListPage.jsx
@@ -1,11 +1,25 @@
 import React from "react";
+import { Row, Col, Container, ListGroup, Button } from "react-bootstrap";
 import ProductCartComponent from "../components/ProductCardComponent";
 
 const ProductListPage = () => {
   return (
     <div>
-      <h1>Product list page</h1>
-      <ProductCartComponent />
+      <Container fluid>
+        <Row>
+          <Col md={3}>
+            <ListGroup variant="flush">
+              <ListGroup.Item>
+                <Button variant="primary">Primary</Button>
+                <Button variant="danger">Danger</Button>
+              </ListGroup.Item>
+            </ListGroup>
+          </Col>
+          <Col md={9}>
+            <ProductCartComponent />
+          </Col>
+        </Row>
+      </Container>
     </div>
   );
 };
