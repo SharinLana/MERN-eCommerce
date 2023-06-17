ccdbeff12c692bd136263b733cef0fe66848edfd
Lana Sharin <lana.sharin.webdev@gmail.com>
2023-06-17 15:29:48 -0700

    render the ProductCartComponent inside of the ProductListPage

--- frontend/src/pages/ProductListPage.jsx
+++ frontend/src/pages/ProductListPage.jsx
@@ -1,9 +1,11 @@
 import React from "react";
+import ProductCartComponent from "../components/ProductCardComponent";
 
 const ProductListPage = () => {
   return (
     <div>
       <h1>Product list page</h1>
+      <ProductCartComponent />
     </div>
   );
 };
