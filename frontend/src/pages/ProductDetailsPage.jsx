c1e1b4da0df62b4ec67be303bdff7ff180eb5ce5
Lana Sharin <lana.sharin.webdev@gmail.com>
2023-06-19 15:39:31 -0700

    add a send review form to the ProductDetailsPage

--- frontend/src/pages/ProductDetailsPage.jsx
+++ frontend/src/pages/ProductDetailsPage.jsx
@@ -7,6 +7,7 @@ import {
   ListGroup,
   Form,
   Button,
+  Alert,
 } from "react-bootstrap";
 import { Rating } from "react-simple-star-rating";
 import AddedToCartMessageComponent from "../components/AddedToCartMessageComponent";
@@ -66,7 +67,28 @@ const ProductDetailsPage = () => {
             </Col>
           </Row>
           <hr />
-          send review form
+          {/* Send review form */}
+          <Alert variant="danger">Login first to write a review</Alert>
+          <Form>
+            <Form.Group className="mb-3" controlId="exampleForm.ControlInput1">
+              <Form.Label>Email address</Form.Label>
+              <Form.Control type="email" placeholder="name@example.com" />
+            </Form.Group>
+            <Form.Group
+              className="mb-3"
+              controlId="exampleForm.ControlTextarea1"
+            >
+              <Form.Label>Example textarea</Form.Label>
+              <Form.Control as="textarea" rows={3} />
+            </Form.Group>
+            <Form.Select aria-label="Default select example">
+              <option>Open this select menu</option>
+              <option value="1">One</option>
+              <option value="2">Two</option>
+              <option value="3">Three</option>
+            </Form.Select>
+            <Button variant="primary">Primary</Button>
+          </Form>
         </Col>
       </Row>
     </Container>
