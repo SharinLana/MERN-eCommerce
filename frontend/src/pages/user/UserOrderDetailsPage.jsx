35b6e5e542fc03ab8cc864445d1c55641b4d113b
Lana Sharin <lana.sharin.webdev@gmail.com>
2023-06-20 12:47:06 -0700

    prepare the UserOrderDetailsPage

--- frontend/src/pages/user/UserOrderDetailsPage.jsx
+++ frontend/src/pages/user/UserOrderDetailsPage.jsx
@@ -1,10 +1,27 @@
 import React from "react";
+import { Container, Row, Col } from "react-bootstrap";
 
 const UserOrderDetailsPage = () => {
   return (
-    <div>
-      <h1>User Order Details page</h1>
-    </div>
+    <Container fluid>
+      <Row className="mt-4">
+        <h1>Order Details</h1>
+        <Col md={8}>
+          <br />
+          <Row>
+            <Col md={6}>
+              <h2>Shipping</h2>
+            </Col>
+            <Col md={6}>
+              <h2>Payment method</h2>
+            </Col>
+          </Row>
+        </Col>
+        <Col md={4}>
+          <h3>Order summary</h3>
+        </Col>
+      </Row>
+    </Container>
   );
 };
 
