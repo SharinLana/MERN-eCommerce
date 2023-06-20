7749965fe77f949745dc2208cdae4681ee1c0550
Lana Sharin <lana.sharin.webdev@gmail.com>
2023-06-19 19:12:04 -0700

    prepare CartPage

--- frontend/src/pages/CartPage.jsx
+++ frontend/src/pages/CartPage.jsx
@@ -1,9 +1,40 @@
 import React from "react";
+import { Row, Col, Container, Alert, ListGroup, Button } from "react-bootstrap";
+import { LinkContainer } from "react-router-bootstrap";
+
 const CartPage = () => {
   return (
-    <div>
-      <h1>Cart page</h1>
-    </div>
+    <Container fluid>
+      <Row className="mt-4">
+        <Col md={8}>
+          <h1>Shopping cart</h1>
+          {Array.from({ length: 3 }).map((item, idx) => (
+            <div key={idx}>
+              CartItemComponent <br />
+            </div>
+          ))}
+          <Alert variant="info">Your cart is empty</Alert>
+        </Col>
+
+        <Col md={4}>
+          <ListGroup>
+            <ListGroup.Item>
+              <h3>Subtotal (2 items) </h3>
+            </ListGroup.Item>
+            <ListGroup.Item>
+              Price: <span className="fw-bold">$879</span>
+            </ListGroup.Item>
+            <ListGroup.Item>
+              <LinkContainer to="/user/order-details">
+                <Button variant="primary" type="button">
+                  Proceed to checkout
+                </Button>
+              </LinkContainer>
+            </ListGroup.Item>
+          </ListGroup>
+        </Col>
+      </Row>
+    </Container>
   );
 };
 
