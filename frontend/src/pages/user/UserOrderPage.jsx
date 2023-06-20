eff2ad298d8e00b2959c7147a8bf89c6d595a42f
Lana Sharin <lana.sharin.webdev@gmail.com>
2023-06-20 12:40:04 -0700

    prepare the UserOrdersPage using react-bootstrap table

--- frontend/src/pages/user/UserOrderPage.jsx
+++ frontend/src/pages/user/UserOrderPage.jsx
@@ -1,11 +1,43 @@
 import React from "react";
+import { Row, Col, Table } from "react-bootstrap";
 
-const UserOrderPage = () => {
+const UserOrdersPage = () => {
   return (
-    <div>
-      <h1>User Order page</h1>
-    </div>
+    <Row className="m-5">
+      <Col md={12}>
+        <h1>My Orders</h1>
+        <Table striped bordered hover>
+          <thead>
+            <tr>
+              <th>#</th>
+              <th>First Name</th>
+              <th>Last Name</th>
+              <th>Username</th>
+            </tr>
+          </thead>
+          <tbody>
+            <tr>
+              <td>1</td>
+              <td>Mark</td>
+              <td>Otto</td>
+              <td>@mdo</td>
+            </tr>
+            <tr>
+              <td>2</td>
+              <td>Jacob</td>
+              <td>Thornton</td>
+              <td>@fat</td>
+            </tr>
+            <tr>
+              <td>3</td>
+              <td colSpan={2}>Larry the Bird</td>
+              <td>@twitter</td>
+            </tr>
+          </tbody>
+        </Table>
+      </Col>
+    </Row>
   );
 };
 
-export default UserOrderPage;
+export default UserOrdersPage;
