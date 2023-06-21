7b1abf0b1ff584f2686c4b2ec24487ed5afa23b8
Lana Sharin <lana.sharin.webdev@gmail.com>
2023-06-21 16:58:54 -0700

    add an error handling middleware to the server.js

--- backend/server.js
+++ backend/server.js
@@ -1,14 +1,22 @@
 require("dotenv").config();
 const express = require("express");
-
-const app = express();
-
 // Routes
 const apiRoutes = require("./routes/apiRoutes");
 
+const app = express();
+
 const port = process.env.PORT || 3000;
 
+// Routes middleware
 app.use("/api", apiRoutes);
+
+// Error middleware
+app.use((error, req, res, next) => {
+  res.status(500).json({
+    message: error.message,
+    stack: error.stack,
+  });
+});
 
 const start = async () => {
   app.listen(port, () => {
