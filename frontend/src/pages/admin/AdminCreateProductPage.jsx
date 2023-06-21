43afd965ec79ea2e3cbb004520353d97f68af8ab
Lana Sharin <lana.sharin.webdev@gmail.com>
2023-06-21 12:29:56 -0700

    prepare the AdminCreateProductPage (basic setup)

--- frontend/src/pages/admin/AdminCreateProductPage.jsx
+++ frontend/src/pages/admin/AdminCreateProductPage.jsx
@@ -1,10 +1,24 @@
 import React from "react";
+import { Row, Col, Container } from "react-bootstrap";
+import { Link } from "react-router-dom";
+
 
 const AdminCreateProductPage = () => {
+
   return (
-    <div>
-      <h1>Admin create product page</h1>
-    </div>
+    <Container>
+      <Row className="justify-content-md-center mt-5">
+        <Col md={1}>
+          <Link to="/admin/products" className="btn btn-info my-3">
+            Go Back
+          </Link>
+        </Col>
+        <Col md={6}>
+          <h1>Create a new product</h1>
+
+        </Col>
+      </Row>
+    </Container>
   );
 };
 
