3a181c08862c9076b42df6a38a65a8d280780b26
Lana Sharin <lana.sharin.webdev@gmail.com>
2023-06-21 13:07:56 -0700

    prepare the AdminChatsPage

--- frontend/src/pages/admin/AdminChatsPage.jsx
+++ frontend/src/pages/admin/AdminChatsPage.jsx
@@ -1,10 +1,19 @@
 import React from "react";
+import AdminLinksComponent from "../../components/AdminLinksComponent";
+import { Row, Col } from "react-bootstrap";
 
 const AdminChatsPage = () => {
   return (
-    <div>
-      <h1>Admin chats page</h1>
-    </div>
+    <Row className="m-5">
+      <Col md={2}>
+        <AdminLinksComponent />
+      </Col>
+      <Col md={10}>
+        <Row>
+          
+        </Row>
+      </Col>
+    </Row>
   );
 };
 
