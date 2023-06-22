86f6c92b7a01dc697d2eb1574747f6d99e3c7e42
Lana Sharin <lana.sharin.webdev@gmail.com>
2023-06-22 16:33:21 -0700

    create OrderModel

---
+++ backend/models/OrderModel.js
@@ -0,0 +1,72 @@
+const mongoose = require("mongoose");
+const User = require("./UserModel");
+
+const orderSchema = mongoose.Schema(
+  {
+    user: {
+      type: mongoose.Schema.Types.ObjectId,
+      required: [true, "The order must be connected to a User"],
+      ref: User,
+    },
+    orderTotal: {
+      itemsCount: {
+        type: Number,
+        required: [true, "Please provide a total number of items"],
+      },
+      cartSubtotal: {
+        type: Number,
+        required: [true, "Please provide a total price"],
+      },
+    },
+    cartItems: [
+      {
+        name: { type: String, required: [true, "Cart item must have a name"] },
+        price: {
+          type: Number,
+          required: [true, "Cart item must have a price"],
+        },
+        image: {
+          path: {
+            type: String,
+            required: [true, "Cart item must have an image"],
+          },
+        },
+        quantity: {
+          type: Number,
+          required: [true, "Cart item must have a quantity"],
+        },
+        count: {
+          type: Number,
+          required: [true, "Cart item must have a subtotal price"],
+        },
+      },
+    ],
+    transactionResult: {
+      status: { type: String },
+      createTime: { type: String },
+      amount: { type: Number },
+    },
+    isPaid: {
+      type: Boolean,
+      required: true,
+      default: false,
+    },
+    paidAt: {
+      type: Date,
+    },
+    isDelivered: {
+      type: Boolean,
+      required: true,
+      default: false,
+    },
+    deliveredAt: {
+      type: Date,
+    },
+  },
+  {
+    timestamps: true,
+  }
+);
+
+const Order = mongoose.model("Order", orderSchema);
+module.exports = Order;
