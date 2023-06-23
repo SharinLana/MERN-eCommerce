c40b42a89782cf74c38907285020e8d370a501b1
Lana Sharin <lana.sharin.webdev@gmail.com>
2023-06-23 16:49:19 -0700

    create an addCategory controller

--- backend/controllers/categoryControllers.js
+++ backend/controllers/categoryControllers.js
@@ -10,4 +10,24 @@ const getCategories = async (req, res, next) => {
   }
 };
 
+const addCategory = async (req, res, next) => {
+  try {
+    const { category } = req.body;
+
+    if (!category) {
+      res.status(400).send("Category input is required");
+    }
+
+    const categoryExists = await Category.findOne({ name: category });
+    if (categoryExists) {
+      res.status(400).send("Category already exists!");
+    } else {
+      const newCategory = await Category.create({ name: category });
+      res.status(201).json(newCategory);
+    }
+  } catch (err) {
+    next(err);
+  }
+};
+
 module.exports = getCategories;
