1476ae779a6c7ff46c3c690ceef9eb7ecc7006cd
Lana Sharin <lana.sharin.webdev@gmail.com>
2023-06-22 18:00:22 -0700

    populate the DB with users data

--- backend/seeder/seeder.js
+++ backend/seeder/seeder.js
@@ -4,23 +4,27 @@ connectToDB();
 const categoryData = require("./categories");
 const productData = require("./products");
 const reviewData = require("./reviews");
+const userData = require("./users");
 const Category = require("../models/CategoryModel");
 const Product = require("../models/ProductModel");
 const Review = require("../models/ReviewModel");
+const User = require("../models/UserModel");
 
 const importData = async () => {
   try {
     await Category.collection.dropIndexes();
     await Product.collection.dropIndexes();
     await Review.collection.dropIndexes();
+    await User.collection.dropIndexes();
 
     await Category.collection.deleteMany({});
     await Product.collection.deleteMany({});
     await Review.collection.deleteMany({});
+    await User.collection.deleteMany({});
 
     await Category.insertMany(categoryData);
 
-    // fill the "reviews" property in the "products" DB collection 
+    // fill the "reviews" property in the "products" DB collection
     // with the data from the "reviews" collection
     const reviews = await Review.insertMany(reviewData);
     const sampleProducts = productData.map((product) => {
@@ -30,6 +34,7 @@ const importData = async () => {
       return { ...product };
     });
     await Product.insertMany(sampleProducts);
+    await User.insertMany(userData);
 
     console.log("Seeder data proceeded successfully");
 
