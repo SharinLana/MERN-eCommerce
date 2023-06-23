6f353ebb625864ef6d07622e396e3c8568a7a03a
Lana Sharin <lana.sharin.webdev@gmail.com>
2023-06-22 17:13:04 -0700

    populate the database with dummy reviews

--- backend/seeder/seeder.js
+++ backend/seeder/seeder.js
@@ -3,19 +3,24 @@ connectToDB();
 
 const categoryData = require("./categories");
 const productData = require("./products");
+const reviewData = require("./reviews");
 const Category = require("../models/CategoryModel");
 const Product = require("../models/ProductModel");
+const Review = require("../models/ReviewModel");
 
 const importData = async () => {
   try {
     await Category.collection.dropIndexes();
     await Product.collection.dropIndexes();
+    await Review.collection.dropIndexes();
 
     await Category.collection.deleteMany({});
     await Product.collection.deleteMany({});
+    await Review.collection.deleteMany({});
 
     await Category.insertMany(categoryData);
     await Product.insertMany(productData);
+    await Review.insertMany(reviewData);
 
     console.log("Seeder data proceeded successfully");
 
