4cba3d245b30385713b699022e2f6af8611704d4
Lana Sharin <lana.sharin.webdev@gmail.com>
2023-06-22 17:01:00 -0700

    populated the mongoDB collections with product dummy data via seeder.js

--- backend/seeder/seeder.js
+++ backend/seeder/seeder.js
@@ -2,13 +2,21 @@ const connectToDB = require("../db_config/db");
 connectToDB();
 
 const categoryData = require("./categories");
+const productData = require("./products");
 const Category = require("../models/CategoryModel");
+const Product = require("../models/ProductModel");
 
 const importData = async () => {
   try {
     await Category.collection.dropIndexes();
+    await Product.collection.dropIndexes();
+
     await Category.collection.deleteMany({});
+    await Product.collection.deleteMany({});
+
     await Category.insertMany(categoryData);
+    await Product.insertMany(productData);
+
     console.log("Seeder data proceeded successfully");
 
     process.exit();
