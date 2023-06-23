c0a0ead090432705c18b904aab0df2b5afce6b01
Lana Sharin <lana.sharin.webdev@gmail.com>
2023-06-23 16:19:59 -0700

    create a getCategories controller in the categoryControllers.js

--- backend/controllers/categoryControllers.js
+++ backend/controllers/categoryControllers.js
@@ -1,5 +1,13 @@
-const getCategories = async (req, res) => {
-  res.send("All categories");
+const Category = require("../models/CategoryModel");
+
+const getCategories = async (req, res, next) => {
+  try {
+    const categories = await Category.find({}).sort({ name: "asc" }).orFail();
+
+    res.status(200).json(categories);
+  } catch (err) {
+    next(err);
+  }
 };
 
 module.exports = getCategories;
