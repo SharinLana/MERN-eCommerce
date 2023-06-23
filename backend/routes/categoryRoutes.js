32b2a90c517581024d46c0a686a18d4c7dc8877c
Lana Sharin <lana.sharin.webdev@gmail.com>
2023-06-23 16:53:50 -0700

    create a post route in categoryRoutes.js

--- backend/routes/categoryRoutes.js
+++ backend/routes/categoryRoutes.js
@@ -1,8 +1,12 @@
 const express = require("express");
-const getCategories = require("../controllers/categoryControllers");
+const {
+  getCategories,
+  addCategory,
+} = require("../controllers/categoryControllers");
 
 const router = express.Router();
 
 router.get("/", getCategories);
+router.post("/", addCategory);
 
 module.exports = router;
