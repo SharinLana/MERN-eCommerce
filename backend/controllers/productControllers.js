c20444a96103e2bed2c5dfcba813f2f4d9e92f06
Lana Sharin <lana.sharin.webdev@gmail.com>
2023-06-24 16:55:54 -0700

    add a limit for items per page in the getProducts controller

--- backend/controllers/productControllers.js
+++ backend/controllers/productControllers.js
@@ -5,7 +5,9 @@ const getProducts = async (req, res, next) => {
   try {
     const pageNum = Number(req.query.pageNum) || 1;
 
-    const products = await Product.find({}).skip(itemsPerPage * (pageNum - 1));
+    const products = await Product.find({})
+      .skip(itemsPerPage * (pageNum - 1))
+      .limit(itemsPerPage);
 
     res.status(200).json({
       status: "success",
