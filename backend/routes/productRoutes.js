6884b4e5021b81e23962e07598705bea603a6881
Lana Sharin <lana.sharin.webdev@gmail.com>
2023-06-25 14:25:07 -0700

    add 2 more routes to the productRoutes.js

--- backend/routes/productRoutes.js
+++ backend/routes/productRoutes.js
@@ -3,7 +3,9 @@ const { getProducts } = require("../controllers/productControllers");
 
 const router = express.Router();
 
+router.get("/category/:categoryName/search/:searchQuery", getProducts);
 router.get("/category/:categoryName", getProducts);
+router.get("/search/:searchQuery", getProducts);
 router.get("/", getProducts);
 
 module.exports = router;
