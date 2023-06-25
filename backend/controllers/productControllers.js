e4500e144be0ad5e9683e4917877182fc1371666
Lana Sharin <lana.sharin.webdev@gmail.com>
2023-06-24 17:54:59 -0700

    implement filtering by price and by rating in the getProducts controller

--- backend/controllers/productControllers.js
+++ backend/controllers/productControllers.js
@@ -3,8 +3,30 @@ const itemsPerPage = require("../utils/pagination");
 
 const getProducts = async (req, res, next) => {
   try {
+    // Filtering
+    let query = {};
+    let queryCondition = false;
+
+    let priceQueryCondition = {};
+    if (req.query.price) {
+      queryCondition = true;
+      query = { price: { $lte: Number(req.query.price) } };
+    }
+
+    let ratingQueryCondition = {};
+    if (req.query.rating) {
+      queryCondition = true;
+      query = { rating: { $in: req.query.rating.split(",") } }; // input: rating: [3, 4, 5] (3 options marked ina row)
+    }
+
+    if (queryCondition) {
+      query = {
+        $and: [priceQueryCondition, ratingQueryCondition],
+      };
+    }
+
+    // Pagination
     const pageNum = Number(req.query.pageNum) || 1;
-    const totalNumOfProducts = await Product.countDocuments({});
 
     // Sort by name, price, etc
     let sort = {};
@@ -15,7 +37,8 @@ const getProducts = async (req, res, next) => {
       sort = { [sortOpt[0]]: Number(sortOpt[1]) }; // (e.g.{ "price": 1}, or { "price": -1})
     }
 
-    const products = await Product.find({})
+    const totalNumOfProducts = await Product.countDocuments(query);
+    const products = await Product.find(query)
       .skip(itemsPerPage * (pageNum - 1))
       .sort(sort)
       .limit(itemsPerPage);
