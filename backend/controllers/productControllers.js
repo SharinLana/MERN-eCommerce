9e2618ccf84fd77ac7afbc9a3cebc1b7c846d082
Lana Sharin <lana.sharin.webdev@gmail.com>
2023-06-24 17:00:42 -0700

    calculate the number of pagination buttons in the getProducts controller

--- backend/controllers/productControllers.js
+++ backend/controllers/productControllers.js
@@ -4,6 +4,7 @@ const itemsPerPage = require("../utils/pagination");
 const getProducts = async (req, res, next) => {
   try {
     const pageNum = Number(req.query.pageNum) || 1;
+    const totalNumOfProducts = await Product.countDocuments({});
 
     const products = await Product.find({})
       .skip(itemsPerPage * (pageNum - 1))
@@ -11,6 +12,7 @@ const getProducts = async (req, res, next) => {
 
     res.status(200).json({
       status: "success",
+      numOfPaginationButtons: Math.ceil(totalNumOfProducts / itemsPerPage),
       products,
     });
   } catch (err) {
