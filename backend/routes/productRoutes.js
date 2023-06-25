023001bcb0093c064c4f5481193889c20e3338fc
Lana Sharin <lana.sharin.webdev@gmail.com>
2023-06-24 17:58:54 -0700

    create a new get route in productRoutes.js to get products by category (through the All dropdown button near the searchbar

--- backend/routes/productRoutes.js
+++ backend/routes/productRoutes.js
@@ -3,6 +3,7 @@ const { getProducts } = require("../controllers/productControllers");
 
 const router = express.Router();
 
+router.get("/category/:categoryName", getProducts);
 router.get("/", getProducts);
 
 module.exports = router;
