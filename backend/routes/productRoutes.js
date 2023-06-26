e737d025c4c63793515b04dd886a392b8f1fd46c
Lana Sharin <lana.sharin.webdev@gmail.com>
2023-06-26 13:28:01 -0700

    create a get products route for admin in productRoutes.js

--- backend/routes/productRoutes.js
+++ backend/routes/productRoutes.js
@@ -3,6 +3,7 @@ const {
   getProducts,
   getProductById,
   getBestsellers,
+  adminGetProducts
 } = require("../controllers/productControllers");
 
 const router = express.Router();
@@ -13,5 +14,8 @@ router.get("/search/:searchQuery", getProducts);
 router.get("/", getProducts);
 router.get("/bestsellers", getBestsellers);
 router.get("/get-one/:id", getProductById);
+
+// Admin routes
+router.get("/admin", adminGetProducts);
 
 module.exports = router;
