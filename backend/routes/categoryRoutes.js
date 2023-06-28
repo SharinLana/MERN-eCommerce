d12d3f29d1fc81f9ef1905b0672fac9a04a475c9
Lana Sharin <lana.sharin.webdev@gmail.com>
2023-06-28 16:01:38 -0700

    set access restriction for categoryRoites

--- backend/routes/categoryRoutes.js
+++ backend/routes/categoryRoutes.js
@@ -5,10 +5,19 @@ const {
   deleteCategory,
   addAttribute,
 } = require("../controllers/categoryControllers");
+const {
+  verifyIsLoggedIn,
+  verifyIsAdmin,
+} = require("../middleware/verifyAuthToken");
 
 const router = express.Router();
 
 router.get("/", getCategories);
+
+// Available for admin only
+router.use(verifyIsLoggedIn); // checking cookies: if it exists or not
+router.use(verifyIsAdmin); // checking if the logged in user is Admin or not
+
 router.post("/", addCategory);
 router.delete("/:category", deleteCategory);
 router.post("/attrs", addAttribute);
