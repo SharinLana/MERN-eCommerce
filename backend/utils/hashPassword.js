709acd61035a763f03e02f7d65aea1befd368933
Lana Sharin <lana.sharin.webdev@gmail.com>
2023-06-28 12:19:16 -0700

    create a util function for hashing password

--- backend/utils/hashPassword.js
+++ backend/utils/hashPassword.js
@@ -1,1 +1,6 @@
-const bcrypt = require("bcryptjs")
+const bcrypt = require("bcryptjs");
+const salt = bcrypt.genSaltSync(10);
+
+const hashPassword = (password) => bcrypt.hashPassword(password, salt);
+
+module.exports = { hashPassword };
