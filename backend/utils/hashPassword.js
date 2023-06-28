f95e6968e694259de4f789804916c6d789ab1082
Lana Sharin <lana.sharin.webdev@gmail.com>
2023-06-28 12:25:19 -0700

    fix a bcrypt method name in ./utils/hashPassword.js

--- backend/utils/hashPassword.js
+++ backend/utils/hashPassword.js
@@ -1,6 +1,6 @@
 const bcrypt = require("bcryptjs");
 const salt = bcrypt.genSaltSync(10);
 
-const hashPassword = (password) => bcrypt.hashPassword(password, salt);
+const hashPassword = (password) => bcrypt.hashSync(password, salt);
 
 module.exports = { hashPassword };
