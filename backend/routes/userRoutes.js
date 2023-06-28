e84ee96819aefcafae50e8b65cf6c0291a4f460e
Lana Sharin <lana.sharin.webdev@gmail.com>
2023-06-28 11:58:33 -0700

    create a new post route in the userRoutes.js to register a user

--- backend/routes/userRoutes.js
+++ backend/routes/userRoutes.js
@@ -1,8 +1,9 @@
 const express = require("express");
-const { getUsers } = require("../controllers/userControllers");
+const { getUsers, registerUser } = require("../controllers/userControllers");
 
 const router = express.Router();
 
 router.get("/", getUsers);
+router.post("/register", registerUser);
 
 module.exports = router;
