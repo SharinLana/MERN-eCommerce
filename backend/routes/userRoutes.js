052d8f1f6cfda4e51c8aa9687e8cf6ef22160694
Lana Sharin <lana.sharin.webdev@gmail.com>
2023-06-29 13:57:39 -0700

    create a route for updating user profile in userRoutes.js + set route restriction middleware

--- backend/routes/userRoutes.js
+++ backend/routes/userRoutes.js
@@ -3,12 +3,27 @@ const {
   getUsers,
   registerUser,
   loginUser,
+  updateUserProfile,
 } = require("../controllers/userControllers");
+const {
+  verifyIsLoggedIn,
+  verifyIsAdmin,
+} = require("../middleware/verifyAuthToken");
 
 const router = express.Router();
 
+// Public routes
 router.get("/", getUsers);
 router.post("/register", registerUser);
 router.post("/login", loginUser);
+
+// Logged in user routes
+router.use(verifyIsLoggedIn);
+
+router.post("/profile", updateUserProfile)
+
+
+// Admin routes
+router.use(verifyIsAdmin);
 
 module.exports = router;
