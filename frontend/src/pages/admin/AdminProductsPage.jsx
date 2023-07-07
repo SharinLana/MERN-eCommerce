d5e330837ebf9d86e8fe5d5e52b9681b21484a7a
Lana Sharin <lana.sharin.webdev@gmail.com>
2023-07-07 16:32:46 -0700

    create a fetchProducts function in the AdminProductsPage and pass it to the ProductsPageComponent

--- frontend/src/pages/admin/AdminProductsPage.jsx
+++ frontend/src/pages/admin/AdminProductsPage.jsx
@@ -1,8 +1,14 @@
 import React from "react";
+import axios from "axios";
 import ProductsPageComponent from "./components/ProductsPageComponent";
 
+const fetchProducts = async () => {
+  const { data } = await axios.get(`/api/products/admin`);
+  return data;
+};
+
 const AdminProductsPage = () => {
-  return <ProductsPageComponent />;
+  return <ProductsPageComponent fetchProducts={fetchProducts} />;
 };
 
 export default AdminProductsPage;
