88681dbd7cc34bda3beed9c6c0450d6632c926ab
Lana Sharin <lana.sharin.webdev@gmail.com>
2023-07-07 14:42:01 -0700

    temporarily desactivate token verification functions

--- backend/middleware/verifyAuthToken.js
+++ backend/middleware/verifyAuthToken.js
@@ -1,6 +1,8 @@
 const jwt = require("jsonwebtoken");
 
 const verifyIsLoggedIn = async (req, res, next) => {
+  next();
+  return; //* to do: remove later
   try {
     const token = req.cookies.access_token;
     // console.log(token)
@@ -24,6 +26,8 @@ const verifyIsLoggedIn = async (req, res, next) => {
 };
 
 const verifyIsAdmin = (req, res, next) => {
+  next();
+  return; //* to do: remove later
   if (req.user && req.user.isAdmin) {
     next();
   } else {
