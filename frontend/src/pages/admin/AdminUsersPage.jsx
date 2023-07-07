e5993ba516e506831120062f019f6d8b8ecef3a8
Lana Sharin <lana.sharin.webdev@gmail.com>
2023-07-07 14:43:13 -0700

    fetch the users from the DB on the AdminUsersPage and pass them to the UsersPageComponent

--- frontend/src/pages/admin/AdminUsersPage.jsx
+++ frontend/src/pages/admin/AdminUsersPage.jsx
@@ -1,8 +1,11 @@
 import React from "react";
+import axios from "axios";
 import UsersPageComponent from "./components/UsersPageComponent";
 
 const AdminUsersPage = () => {
-  return <UsersPageComponent />;
+  const { data } = axios.get("/api/users");
+
+  return <UsersPageComponent users={data} />;
 };
 
 export default AdminUsersPage;
