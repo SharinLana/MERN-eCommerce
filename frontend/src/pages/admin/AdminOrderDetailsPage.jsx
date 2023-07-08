211130a417b5098fe805b7f93393b7915e3361e8
Lana Sharin <lana.sharin.webdev@gmail.com>
2023-07-07 18:43:44 -0700

    create a getOrderDetails function in the AdminOrderDetailsPage.js and pass its returned value to the OrderDetailsComponent

--- frontend/src/pages/admin/AdminOrderDetailsPage.jsx
+++ frontend/src/pages/admin/AdminOrderDetailsPage.jsx
@@ -1,9 +1,14 @@
 import React from "react";
-// import axios from "axios";
+import axios from "axios";
 import OrderDetailsComponent from "./components/OrderDetailsComponent";
 
+const getOrderDetails = async (orderId) => {
+  const { data } = await axios.get(`/api/orders/user/${orderId}`);
+  return data;
+};
+
 const AdminOrderDetailsPage = () => {
-  return <OrderDetailsComponent />;
+  return <OrderDetailsComponent getOrderDetails={getOrderDetails} />;
 };
 
 export default AdminOrderDetailsPage;
