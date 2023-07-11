4c5f332288a7e38b3cc7fbc0a6dbce9789fc5c3f
Lana Sharin <lana.sharin.webdev@gmail.com>
2023-07-10 17:35:03 -0700

    store the logged in user data in the local storage or session storage depending on the status of the Do Not Logout checkbox

--- frontend/src/pages/LoginPage.jsx
+++ frontend/src/pages/LoginPage.jsx
@@ -11,6 +11,15 @@ const loginUserApiRequest = async (email, password, doNotLogout) => {
     doNotLogout,
   });
 
+  // Store the logged in user info in the local storage if the "Do Not Logout" box was checked
+  if (data.userLoggedIn.doNotLogout) {
+    // data will remain in the local storage even after closing the browser and reopening it
+    localStorage.setItem("userInfo", JSON.stringify(data.userLoggedIn));
+  } else {
+    // data will remain in the session storage only until closing browser 
+    sessionStorage.setItem("userInfo", JSON.stringify(data.userLoggedIn));
+  }
+
   return data;
 };
 
