eb6134e25287758f121231376c1b0b9e59f6316c
Lana Sharin <lana.sharin.webdev@gmail.com>
2023-07-11 16:16:53 -0700

    finalize the ProtectedRoutesComponent

--- frontend/src/components/ProtectedRoutesComponent.jsx
+++ frontend/src/components/ProtectedRoutesComponent.jsx
@@ -1,21 +1,63 @@
-import React from "react";
 import { Outlet, Navigate } from "react-router-dom";
 import UserChatComponent from "./user/UserChatComponent";
 
-const ProtectedRoutesComponent = ({ admin }) => {
-  if (admin) {
-    let adminAuth = true;
-    return adminAuth ? <Outlet /> : <Navigate to="/login" />;
-  } else {
-    let userAuth = true;
-    return userAuth ? (
+import axios from "axios";
+import React, { useEffect, useState } from "react";
+import LoginPage from "../pages/LoginPage";
+
+const ProtectedRoutesComponent = ({ adminRoute }) => {
+  const [isAuth, setIsAuth] = useState();
+
+  useEffect(() => {
+    axios
+      .get("/api/get-token")
+      .then(function (data) {
+        if (data.data.token) {
+          setIsAuth(data.data.token);
+        }
+        return isAuth;
+      })
+      .catch((err) => {
+        if (err.response.data === "Unauthorized. Invalid Token") {
+          setIsAuth();
+          return isAuth;
+        }
+      });
+  }, [isAuth]);
+
+  // if the user is not logged in
+  if (isAuth === undefined) return <LoginPage />;
+
+  // if the user is logged in, NOT and admin and trying to access admin's routes
+  else if (isAuth && adminRoute && isAuth !== "admin")
+    return (
       <>
-        <UserChatComponent /> <Outlet />
+        <Navigate to="/login" />
+        <LoginPage />
       </>
-    ) : (
-      <Navigate to="/login" />
     );
-  }
+
+  // if the user is logged in, is admin and trying to access admin's routes
+  else if (isAuth && adminRoute) return <Outlet />;
+
+  // if the user is logged in, is admin and trying to access user's protected routes
+  else if (isAuth && !adminRoute)
+    return (
+      <>
+        <UserChatComponent />
+        {/* all routes inside of the protected user route */}
+        <Outlet />
+      </>
+    );
+    
+  // other cases
+  else
+    return (
+      <>
+        <Navigate to="/login" />
+        <LoginPage />
+      </>
+    );
 };
 
 export default ProtectedRoutesComponent;
