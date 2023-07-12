48478aac451b3e0d6c246c8ecf9fc60a8688ae07
Lana Sharin <lana.sharin.webdev@gmail.com>
2023-07-12 13:05:24 -0700

    create a registerUserApiRequest function in the RegisterPage.js

--- frontend/src/pages/RegisterPage.jsx
+++ frontend/src/pages/RegisterPage.jsx
@@ -1,8 +1,22 @@
 import React from "react";
+import axios from "axios";
 import RegisterPageComponent from "./components/RegisterPageComponent";
 
+const registerUserApiRequest = async (name, lastName, email, password) => {
+  const { data } = await axios.post("/api/users/register", {
+    name,
+    lastName,
+    email,
+    password,
+  });
+
+  return data;
+};
+
 const RegisterPage = () => {
-  return <RegisterPageComponent />;
+  return (
+    <RegisterPageComponent registerUserApiRequest={registerUserApiRequest} />
+  );
 };
 
 export default RegisterPage;
