4aaed672754b510d1881adaaffdde67aec33feba
Lana Sharin <lana.sharin.webdev@gmail.com>
2023-07-15 17:35:08 -0700

    remove user.email field from the updateUserProfile controller in the userControllers.js

--- backend/controllers/userControllers.js
+++ backend/controllers/userControllers.js
@@ -141,7 +141,6 @@ const updateUserProfile = async (req, res, next) => {
 
     user.name = req.body.name || user.name;
     user.lastName = req.body.lastName || user.lastName;
-    user.email = req.body.email || user.email;
     user.phoneNumber = req.body.phoneNumber;
     user.address = req.body.address;
     user.country = req.body.country;
