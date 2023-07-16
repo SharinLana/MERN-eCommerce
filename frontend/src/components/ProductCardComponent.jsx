d91e3944bafee5d0845deccdedae1b77d4e5cc21
Lana Sharin <lana.sharin.webdev@gmail.com>
2023-07-16 15:06:03 -0700

    display the product info dynamically in the ProductCardComponent

--- frontend/src/components/ProductCardComponent.jsx
+++ frontend/src/components/ProductCardComponent.jsx
@@ -3,32 +3,34 @@ import { Card, Button, Row, Col } from "react-bootstrap";
 import { LinkContainer } from "react-router-bootstrap";
 import { Rating } from "react-simple-star-rating";
 
-const ProductCardComponent = ({ images, idx }) => {
+const ProductCardComponent = ({
+  productId,
+  name,
+  description,
+  price,
+  images,
+  rating,
+  reviewsNumber,
+}) => {
   return (
     <Card style={{ marginTop: "30px", marginBottom: "50px" }}>
       <Row>
         {/* Product Image */}
         <Col lg={5}>
-          <Card.Img
-            variant="top"
-            src={"/images/" + images[idx] + "-category.png"}
-          />
+          <Card.Img variant="top" src={images[0] ? images[0].path : ""} />
         </Col>
         {/* Product Description */}
         <Col lg={7}>
           <Card.Body>
-            <Card.Title>Card Title</Card.Title>
+            <Card.Title>{name}</Card.Title>
+            <Card.Text>{description}</Card.Text>
             <Card.Text>
-              {` Some quick example text to build on the card title and make up the
-          bulk of the card's content.`}
+              <Rating readonly size={20} initialValue={rating} />(
+              {reviewsNumber})
             </Card.Text>
             <Card.Text>
-              <Rating readonly size={20} initialValue={5} />
-              (1)
-            </Card.Text>
-            <Card.Text>
-              $124{" "}
-              <LinkContainer to="/product-details">
+              ${price}{" "}
+              <LinkContainer to={`/product-details/${productId}`}>
                 <Button variant="danger">See product</Button>
               </LinkContainer>
             </Card.Text>
