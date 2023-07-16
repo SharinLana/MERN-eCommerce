de85bac0971e75c249152dd86856307de40de69d
Lana Sharin <lana.sharin.webdev@gmail.com>
2023-07-16 14:38:02 -0700

    create a function getAllProducts in the ProductListPage.js to make an api call to the server to get all the products

--- frontend/src/pages/ProductListPage.jsx
+++ frontend/src/pages/ProductListPage.jsx
@@ -1,8 +1,14 @@
 import React from "react";
+import axios from "axios";
 import ProductListPageComponent from "./components/ProductListPageComponent";
 
+const getAllProducts = async () => {
+  const { data } = await axios.get("/api/products");
+  return data;
+};
+
 const ProductListPage = () => {
-  return <ProductListPageComponent />;
+  return <ProductListPageComponent getAllProducts={getAllProducts} />;
 };
 
 export default ProductListPage;
