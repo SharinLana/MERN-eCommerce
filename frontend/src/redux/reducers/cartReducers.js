14bae8a5a2314671bd8eea0d63b9369c981d0a85
Lana Sharin <lana.sharin.webdev@gmail.com>
2023-07-17 18:55:18 -0700

    modify the logix of the case actionTypes.ADD_TO_CART in the cartReducers.js

--- frontend/src/redux/reducers/cartReducers.js
+++ frontend/src/redux/reducers/cartReducers.js
@@ -1,3 +1,4 @@
+/* eslint-disable no-case-declarations */
 import * as actionTypes from "../actionTypes/cart";
 
 const CART_INITIAL_STATE = {
@@ -9,8 +10,52 @@ const CART_INITIAL_STATE = {
 export const cartReducer = (state = CART_INITIAL_STATE, action) => {
   switch (action.type) {
     case actionTypes.ADD_TO_CART:
-      console.log(action.payload);
-      return state;
+      const productBeingAddedToCart = action.payload;
+
+      const productAlreadyExistsInState = state.cartItems.find(
+        (x) => x.productId === productBeingAddedToCart.productId
+      );
+
+      // We cannot change the state directly in Classic Redux
+      // we can only do that through a separate variable (currentState, in this case)
+      const currentState = { ...state };
+
+      if (productAlreadyExistsInState) {
+        // To avoid product duplicates in the cart:
+        // Find this product in the cartItems array and change the total quantity and
+        // total amount accordingly
+        currentState.itemsCount = 0;
+        currentState.cartSubtotal = 0;
+        currentState.cartItems = state.cartItems.map((item) => {
+          if (item.productId === productAlreadyExistsInState.productId) {
+            currentState.itemsCount += Number(productBeingAddedToCart.quantity);
+
+            const sum =
+              Number(productBeingAddedToCart.quantity) *
+              Number(productBeingAddedToCart.price);
+            currentState.cartSubtotal += sum;
+          } else {
+            currentState.itemsCount += Number(item.quantity);
+            const sum = Number(item.quantity) * Number(item.price);
+            currentState.cartSubtotal += sum;
+          }
+          return item.productId === productAlreadyExistsInState.productId
+            ? productBeingAddedToCart
+            : item;
+        });
+      } else {
+        // Re-calculate the total quantity and cart subtotal and add
+        // the new product to the array of products
+        currentState.itemsCount += Number(productBeingAddedToCart.quantity);
+        const sum =
+          Number(productBeingAddedToCart.quantity) *
+          Number(productBeingAddedToCart.price);
+        currentState.cartSubtotal += sum;
+        currentState.cartItems = [...state.cartItems, productBeingAddedToCart];
+      }
+
+      // return modified state
+      return currentState;
     default:
       return state;
   }
