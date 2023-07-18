f97db8ae75ecb235d6b388ff132440040b0e2ba7
Lana Sharin <lana.sharin.webdev@gmail.com>
2023-07-18 14:34:37 -0700

    fix the payload property values in the cartActions.js

--- frontend/src/redux/actions/cartActions.js
+++ frontend/src/redux/actions/cartActions.js
@@ -3,15 +3,16 @@ import axios from "axios";
 
 export const addToCart = (productId, quantity) => async (dispatch, getState) => {
   const { data } = await axios.get(`/api/products/get-one/${productId}`);
+  console.log(data.product)
 
   dispatch({
     type: actionTypes.ADD_TO_CART,
     payload: {
-      productId: data._id,
-      name: data.name,
-      price: data.price,
-      image: data.images[0] ?? null,
-      count: data.count,
+      productId: data.product._id,
+      name: data.product.name,
+      price: data.product.price,
+      image: data.product.images[0] ?? null,
+      count: data.product.count,
       quantity,
     },
   });
