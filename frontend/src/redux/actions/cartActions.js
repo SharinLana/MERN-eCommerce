a39c4f37ef9d99064a6d8dfad4e64ed14077a4d2
Lana Sharin <lana.sharin.webdev@gmail.com>
2023-07-18 16:47:36 -0700

    create a removeFromCart action

--- frontend/src/redux/actions/cartActions.js
+++ frontend/src/redux/actions/cartActions.js
@@ -1,20 +1,31 @@
 import * as actionTypes from "../actionTypes/cart";
 import axios from "axios";
 
-export const addToCart = (productId, quantity) => async (dispatch, getState) => {
-  const { data } = await axios.get(`/api/products/get-one/${productId}`);
+export const addToCart =
+  (productId, quantity) => async (dispatch, getState) => {
+    const { data } = await axios.get(`/api/products/get-one/${productId}`);
 
-  dispatch({
-    type: actionTypes.ADD_TO_CART,
-    payload: {
-      productId: data.product._id,
-      name: data.product.name,
-      price: data.product.price,
-      image: data.product.images[0] ?? null,
-      count: data.product.count,
-      quantity,
-    },
-  });
+    dispatch({
+      type: actionTypes.ADD_TO_CART,
+      payload: {
+        productId: data.product._id,
+        name: data.product.name,
+        price: data.product.price,
+        image: data.product.images[0] ?? null,
+        count: data.product.count,
+        quantity,
+      },
+    });
 
-  localStorage.setItem("cart", JSON.stringify(getState().cart.cartItems));
-};
+    localStorage.setItem("cart", JSON.stringify(getState().cart.cartItems));
+  };
+
+export const removeFromCart =
+  (productId, quantity, price) => (dispatch, getState) => {
+    dispatch({
+      type: actionTypes.REMOVE_FROM_CART,
+      payload: { productId: productId, quantity: quantity, price: price },
+    });
+
+    localStorage.setItem("cart", JSON.stringify(getState().cart.cartItems));
+  };
