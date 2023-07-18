fea4280f13260f871df11872b275d87929a851d9
Lana Sharin <lana.sharin.webdev@gmail.com>
2023-07-18 16:09:29 -0700

    fetch the cart state from Redux and pass it to the CartPageComponent

--- frontend/src/pages/CartPage.jsx
+++ frontend/src/pages/CartPage.jsx
@@ -4,7 +4,18 @@ import { addToCart } from "../redux/actions/cartActions";
 import CartPageComponent from "./components/CartPageComponent";
 
 const CartPage = () => {
-  return <CartPageComponent />;
+  const cartItems = useSelector((state) => state.cart.cartItems);
+  const cartSubtotal = useSelector((state) => state.cart.cartSubtotal);
+  const dispatch = useDispatch();
+
+  return (
+    <CartPageComponent
+      addToCart={addToCart}
+      cartItems={cartItems}
+      cartSubtotal={cartSubtotal}
+      dispatch={dispatch}
+    />
+  );
 };
 
 export default CartPage;
