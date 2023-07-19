7d5c3a3a2efe86d0ee5518a10fa3c9d2b1f9ced8
Lana Sharin <lana.sharin.webdev@gmail.com>
2023-07-19 15:07:56 -0700

    pass dispatch as a prop to the UserCartDetailsPageComponent

--- frontend/src/pages/user/UserCartDetailsPage.jsx
+++ frontend/src/pages/user/UserCartDetailsPage.jsx
@@ -1,17 +1,19 @@
 import React from "react";
-import { useSelector } from "react-redux";
+import { useSelector, useDispatch } from "react-redux";
 import UserCartDetailsPageComponent from "./components/UserCartDetailPageComponent";
 
 const UserCartDetailsPage = () => {
   const cartItems = useSelector((state) => state.cart.cartItems);
   const itemsCount = useSelector((state) => state.cart.itemsCount);
   const cartSubtotal = useSelector((state) => state.cart.cartSubtotal);
+  const dispatch = useDispatch()
 
   return (
     <UserCartDetailsPageComponent
       cartItems={cartItems}
       itemsCount={itemsCount}
       cartSubtotal={cartSubtotal}
+      dispatch={dispatch}
     />
   );
 };
