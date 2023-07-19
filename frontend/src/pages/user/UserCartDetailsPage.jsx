63b34e935527dc28a88be11175a489e738716396
Lana Sharin <lana.sharin.webdev@gmail.com>
2023-07-19 14:54:20 -0700

    fetch the cart state properties from Redux state and pass them to the UserCartDetailsPageComponent

--- frontend/src/pages/user/UserCartDetailsPage.jsx
+++ frontend/src/pages/user/UserCartDetailsPage.jsx
@@ -1,8 +1,19 @@
 import React from "react";
+import { useSelector } from "react-redux";
 import UserCartDetailsPageComponent from "./components/UserCartDetailPageComponent";
 
 const UserCartDetailsPage = () => {
-  return <UserCartDetailsPageComponent />;
+  const cartItems = useSelector((state) => state.cart.cartItems);
+  const itemsCount = useSelector((state) => state.cart.itemsCount);
+  const cartSubtotal = useSelector((state) => state.cart.cartSubtotal);
+
+  return (
+    <UserCartDetailsPageComponent
+      cartItems={cartItems}
+      itemsCount={itemsCount}
+      cartSubtotal={cartSubtotal}
+    />
+  );
 };
 
 export default UserCartDetailsPage;
