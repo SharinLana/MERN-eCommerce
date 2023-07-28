7f75ef9a02f74d3784fba412b4f26818be577873
Lana Sharin <lana.sharin.webdev@gmail.com>
2023-07-28 15:26:40 -0700

    fetch the user orders from the db and pass them as a prop to the UserOrdersPageComponent

--- frontend/src/pages/user/UserOrderPage.jsx
+++ frontend/src/pages/user/UserOrderPage.jsx
@@ -1,9 +1,15 @@
 import React from "react";
 import UserOrdersPageComponent from "./components/UserOrdersPageComponent";
+import axios from "axios";
+
+const getOrders = async () => {
+  const { data } = await axios.get("/api/orders");
+  return data;
+};
 
 
 const UserOrdersPage = () => {
-  return <UserOrdersPageComponent  />;
+  return <UserOrdersPageComponent getOrders={getOrders} />;
 };
 
 export default UserOrdersPage;
