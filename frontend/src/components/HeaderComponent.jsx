95bbb26d8931bc4202e67d2a92d20e3b8a6ed0fa
Lana Sharin <lana.sharin.webdev@gmail.com>
2023-07-28 15:56:08 -0700

    get categories on the HeaderComponent load

--- frontend/src/components/HeaderComponent.jsx
+++ frontend/src/components/HeaderComponent.jsx
@@ -1,4 +1,4 @@
-import React from "react";
+import React, { useEffect } from "react";
 import {
   Navbar,
   Nav,
@@ -17,11 +17,16 @@ import { Link } from "react-router-dom";
 
 import { logout } from "../redux/actions/userActions";
 import { useDispatch, useSelector } from "react-redux";
+import { getCategories } from "../redux/actions/categoryActions";
 
 const HeaderComponent = () => {
   const dispatch = useDispatch();
   const { userInfo } = useSelector((state) => state.userRegisterLogin);
   const itemsCount = useSelector((state) => state.cart.itemsCount);
+
+  useEffect(() => {
+    dispatch(getCategories());
+  }, [dispatch]);
 
   return (
     <Navbar collapseOnSelect expand="lg" bg="dark" variant="dark">
