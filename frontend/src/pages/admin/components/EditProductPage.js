5743d477823d630deeabf3727aab9a91df8d423a
Lana Sharin <lana.sharin.webdev@gmail.com>
2023-07-29 17:29:59 -0700

    display attributes for Edit Product Page

--- frontend/src/pages/admin/components/EditProductPage.js
+++ frontend/src/pages/admin/components/EditProductPage.js
@@ -1,4 +1,4 @@
-import React, { useState, useEffect } from "react";
+import React, { useState, useEffect, Fragment } from "react";
 import {
   Row,
   Col,
@@ -34,8 +34,8 @@ const EditProductPageComponent = ({
     error: "",
   });
   const { id } = useParams();
-
   const navigate = useNavigate();
+  const [attributesFromDb, setAttributesFromDb] = useState([]);
 
   useEffect(() => {
     fetchProduct(id)
@@ -73,6 +73,28 @@ const EditProductPageComponent = ({
 
     setValidated(true);
   };
+
+  useEffect(() => {
+    let categoryOfEditedProduct = categories.find(
+      (item) => item.name === product.category
+    );
+
+    if (categoryOfEditedProduct) {
+      const mainCategoryOfEditedProduct =
+        categoryOfEditedProduct.name.split("/")[0];
+      const mainCategoryOfEditedProductAllData = categories.find(
+        (categoryOfEditedProduct) =>
+          categoryOfEditedProduct.name === mainCategoryOfEditedProduct
+      );
+      if (
+        mainCategoryOfEditedProductAllData &&
+        mainCategoryOfEditedProductAllData.attrs.length > 0
+      ) {
+        setAttributesFromDb(mainCategoryOfEditedProductAllData.attrs);
+      }
+    }
+  }, [product]);
+
   return (
     <Container>
       <Row className="justify-content-md-center mt-5">
@@ -147,34 +169,40 @@ const EditProductPageComponent = ({
               </Form.Select>
             </Form.Group>
 
-            <Row className="mt-5">
-              <Col md={6}>
-                <Form.Group className="mb-3" controlId="formBasicAttributes">
-                  <Form.Label>Choose atrribute and set value</Form.Label>
-                  <Form.Select
-                    name="atrrKey"
-                    aria-label="Default select example"
+            {attributesFromDb.length > 0 && (
+              <Row className="mt-5">
+                <Col md={6}>
+                  <Form.Group className="mb-3" controlId="formBasicAttributes">
+                    <Form.Label>Choose atrribute and set value</Form.Label>
+                    <Form.Select
+                      name="atrrKey"
+                      aria-label="Default select example"
+                    >
+                      <option>Choose attribute</option>
+                      {attributesFromDb.map((item, idx) => (
+                        <Fragment key={idx}>
+                          <option value={item.key}>{item.key}</option>
+                        </Fragment>
+                      ))}
+                    </Form.Select>
+                  </Form.Group>
+                </Col>
+                <Col md={6}>
+                  <Form.Group
+                    className="mb-3"
+                    controlId="formBasicAttributeValue"
                   >
-                    <option>Choose attribute</option>
-                    <option value="red">color</option>
-                  </Form.Select>
-                </Form.Group>
-              </Col>
-              <Col md={6}>
-                <Form.Group
-                  className="mb-3"
-                  controlId="formBasicAttributeValue"
-                >
-                  <Form.Label>Attribute value</Form.Label>
-                  <Form.Select
-                    name="atrrVal"
-                    aria-label="Default select example"
-                  >
-                    <option>Choose attribute value</option>
-                  </Form.Select>
-                </Form.Group>
-              </Col>
-            </Row>
+                    <Form.Label>Attribute value</Form.Label>
+                    <Form.Select
+                      name="atrrVal"
+                      aria-label="Default select example"
+                    >
+                      <option>Choose attribute value</option>
+                    </Form.Select>
+                  </Form.Group>
+                </Col>
+              </Row>
+            )}
 
             <Row>
               <Table hover>
@@ -260,260 +288,3 @@ const EditProductPageComponent = ({
 };
 
 export default EditProductPageComponent;
-
-
-
-// import {
-//   Row,
-//   Col,
-//   Container,
-//   Form,
-//   Button,
-//   CloseButton,
-//   Table,
-//   Alert,
-//   Image,
-// } from "react-bootstrap";
-// import { Link } from "react-router-dom";
-// import { useNavigate } from "react-router-dom";
-// import { useParams } from "react-router-dom";
-
-// const onHover = {
-//   cursor: "pointer",
-//   position: "absolute",
-//   left: "5px",
-//   top: "-10px",
-//   transform: "scale(2.7)",
-// };
-
-// const EditProductPageComponent = ({
-//   categories,
-//   fetchProduct,
-//   updateProductApiRequest,
-// }) => {
-//   const [validated, setValidated] = useState(false);
-//   const [product, setProduct] = useState({});
-//   const [updateProductResponseState, setUpdateProductResponseState] = useState({
-//     message: "",
-//     error: "",
-//   });
-//   const { id } = useParams();
-//   const navigate = useNavigate();
-
-//   useEffect(() => {
-//     fetchProduct(id)
-//       .then((product) => setProduct(product))
-//       .catch((err) => console.log(err));
-//   }, [id]);
-
-//   const handleSubmit = (e) => {
-//     e.preventDefault();
-//     e.stopPropagation();
-
-//     const formInputs = {
-//       name: e.currentTarget.name.value,
-//       description: e.currentTarget.description.value,
-//       count: e.currentTarget.count.value,
-//       price: e.currentTarget.price.value,
-//       category: e.currentTarget.category.value,
-//     };
-
-//     if (event.currentTarget.checkValidity() === true) {
-//       updateProductApiRequest(id, formInputs)
-//         .then((data) => {
-//           if (data.message === "Product was successfully updated")
-//             navigate("/admin/products");
-//         })
-//         .catch((er) =>
-//           setUpdateProductResponseState({
-//             error: er.response.data.message
-//               ? er.response.data.message
-//               : er.response.data,
-//           })
-//         );
-//     }
-
-//     setValidated(true);
-//   };
-
-//   return (
-//     <Container>
-//       <Row className="justify-content-md-center mt-5">
-//         <Col md={1}>
-//           <Link to="/admin/products" className="btn btn-info my-3">
-//             Go Back
-//           </Link>
-//         </Col>
-//         <Col md={6}>
-//           <h1>Edit product</h1>
-//           <Form noValidate validated={validated} onSubmit={handleSubmit}>
-//             <Form.Group className="mb-3" controlId="formBasicName">
-//               <Form.Label>Name</Form.Label>
-//               <Form.Control
-//                 name="name"
-//                 required
-//                 type="text"
-//                 defaultValue={product.name}
-//               />
-//             </Form.Group>
-
-//             <Form.Group
-//               className="mb-3"
-//               controlId="exampleForm.ControlTextarea1"
-//             >
-//               <Form.Label>Description</Form.Label>
-//               <Form.Control
-//                 name="description"
-//                 required
-//                 as="textarea"
-//                 rows={3}
-//                 defaultValue={product.description}
-//               />
-//             </Form.Group>
-//             <Form.Group className="mb-3" controlId="formBasicCount">
-//               <Form.Label>Count in stock</Form.Label>
-//               <Form.Control
-//                 name="count"
-//                 required
-//                 type="number"
-//                 defaultValue={product.count}
-//               />
-//             </Form.Group>
-//             <Form.Group className="mb-3" controlId="formBasicPrice">
-//               <Form.Label>Price</Form.Label>
-//               <Form.Control
-//                 name="price"
-//                 required
-//                 type="text"
-//                 defaultValue={product.price}
-//               />
-//             </Form.Group>
-//             <Form.Group className="mb-3" controlId="formBasicCategory">
-//               <Form.Label>Category</Form.Label>
-//               <Form.Select
-//                 required
-//                 name="category"
-//                 aria-label="Default select example"
-//               >
-//                 <option value="">Choose category</option>
-//                 {categories.map((category, idx) => (
-//                   <option key={idx} value={category.name}>
-//                     {category.name}
-//                   </option>
-//                 ))}
-//               </Form.Select>
-//             </Form.Group>
-
-//             <Row className="mt-5">
-//               <Col md={6}>
-//                 <Form.Group className="mb-3" controlId="formBasicAttributes">
-//                   <Form.Label>Choose atrribute and set value</Form.Label>
-//                   <Form.Select
-//                     name="atrrKey"
-//                     aria-label="Default select example"
-//                   >
-//                     <option>Choose attribute</option>
-//                     <option value="red">color</option>
-//                   </Form.Select>
-//                 </Form.Group>
-//               </Col>
-//               <Col md={6}>
-//                 <Form.Group
-//                   className="mb-3"
-//                   controlId="formBasicAttributeValue"
-//                 >
-//                   <Form.Label>Attribute value</Form.Label>
-//                   <Form.Select
-//                     name="atrrVal"
-//                     aria-label="Default select example"
-//                   >
-//                     <option>Choose attribute value</option>
-//                   </Form.Select>
-//                 </Form.Group>
-//               </Col>
-//             </Row>
-
-//             <Row>
-//               <Table hover>
-//                 <thead>
-//                   <tr>
-//                     <th>Attribute</th>
-//                     <th>Value</th>
-//                     <th>Delete</th>
-//                   </tr>
-//                 </thead>
-//                 <tbody>
-//                   <tr>
-//                     <td>attr key</td>
-//                     <td>attr value</td>
-//                     <td>
-//                       <CloseButton />
-//                     </td>
-//                   </tr>
-//                 </tbody>
-//               </Table>
-//             </Row>
-
-//             <Row>
-//               <Col md={6}>
-//                 <Form.Group className="mb-3" controlId="formBasicNewAttribute">
-//                   <Form.Label>Create new attribute</Form.Label>
-//                   <Form.Control
-//                     disabled={false}
-//                     placeholder="first choose or create category"
-//                     name="newAttrValue"
-//                     type="text"
-//                   />
-//                 </Form.Group>
-//               </Col>
-//               <Col md={6}>
-//                 <Form.Group
-//                   className="mb-3"
-//                   controlId="formBasicNewAttributeValue"
-//                 >
-//                   <Form.Label>Attribute value</Form.Label>
-//                   <Form.Control
-//                     disabled={false}
-//                     placeholder="first choose or create category"
-//                     required={true}
-//                     name="newAttrValue"
-//                     type="text"
-//                   />
-//                 </Form.Group>
-//               </Col>
-//             </Row>
-
-//             <Alert variant="primary">
-//               After typing attribute key and value press enter on one of the
-//               field
-//             </Alert>
-
-//             <Form.Group controlId="formFileMultiple" className="mb-3 mt-3">
-//               <Form.Label>Images</Form.Label>
-//               <Row>
-//                 {product.images &&
-//                   product.images.map((image, idx) => (
-//                     <Col key={idx} style={{ position: "relative" }} xs={3}>
-//                       <Image
-//                         crossOrigin="anonymous"
-//                         src={image.path ?? null}
-//                         fluid
-//                       />
-//                       <i style={onHover} className="bi bi-x text-danger"></i>
-//                     </Col>
-//                   ))}
-//               </Row>
-//               <Form.Control required type="file" multiple />
-//             </Form.Group>
-//             <Button variant="primary" type="submit">
-//               UPDATE
-//             </Button>
-//             {updateProductResponseState.error ?? ""}
-//           </Form>
-//         </Col>
-//       </Row>
-//     </Container>
-//   );
-// };
-
-// export default EditProductPageComponent;
