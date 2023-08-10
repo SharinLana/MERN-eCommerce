69ecd7ad347ea01448139d8339ad98bebf85a764
Lana Sharin <lana.sharin.webdev@gmail.com>
2023-08-10 16:50:47 -0700

    pass the redux categories, dispatch and redux action function  newCategories to the CreateProductPageComponent

--- frontend/src/pages/admin/AdminCreateProductPage.jsx
+++ frontend/src/pages/admin/AdminCreateProductPage.jsx
@@ -6,17 +6,26 @@ import {
   uploadImagesCloudinaryApiRequest,
 } from "./utils/utils";
 
+import { useSelector, useDispatch } from "react-redux";
+import { newCategory } from "../../redux/actions/categoryActions";
+
 const createProductApiRequest = async (formInputs) => {
   const { data } = await axios.post(`/api/products/admin`, { ...formInputs });
   return data;
 };
 
 const AdminCreateProductPage = () => {
+  const { categories } = useSelector((state) => state.getCategories);
+  const dispatch = useDispatch();
+
   return (
     <CreateProductPageComponent
       createProductApiRequest={createProductApiRequest}
       uploadImagesApiRequest={uploadImagesApiRequest}
       uploadImagesCloudinaryApiRequest={uploadImagesCloudinaryApiRequest}
+      categories={categories}
+      dispatch={dispatch}
+      newCategory={newCategory}
     />
   );
 };
