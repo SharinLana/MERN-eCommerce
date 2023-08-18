e4dc72a7f469f1b2dff4422dfbb3cb3daa356632
Lana Sharin <lana.sharin.webdev@gmail.com>
2023-08-17 18:09:21 -0700

    display the select form of the product count dynamically

--- frontend/src/pages/components/ProductDetailsPageComponent.js
+++ frontend/src/pages/components/ProductDetailsPageComponent.js
@@ -130,9 +130,11 @@ const ProductDetailsPageComponent = ({
                         style={{ zIndex: 2000 }}
                       >
                         <option>Choose:</option>
-                        <option value="1">1</option>
-                        <option value="2">2</option>
-                        <option value="3">3</option>
+                        {[...Array(product.count).keys()].map((number) => (
+                          <option key={number + 1} value={number + 1}>
+                            {number + 1}
+                          </option>
+                        ))}
                       </Form.Select>
                     </ListGroup.Item>
                     <ListGroup.Item>
