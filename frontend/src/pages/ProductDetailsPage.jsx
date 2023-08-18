c46e15ebd834355c38922bc8c7941c781a322de8
Lana Sharin <lana.sharin.webdev@gmail.com>
2023-08-17 17:08:26 -0700

    start getting product details

--- frontend/src/pages/ProductDetailsPage.jsx
+++ frontend/src/pages/ProductDetailsPage.jsx
@@ -1,8 +1,14 @@
 import React from "react";
+import axios from "axios";
 import ProductDetailsPageComponent from "./components/ProductDetailsPageComponent";
 
 import { useDispatch } from "react-redux";
 import { addToCart } from "../redux/actions/cartActions";
+
+const getProductDetails = async (id) => {
+  const { data } = await axios.get(`/api/products/get-one/${id}`);
+  return data;
+};
 
 const ProductDetailsPage = () => {
   const dispatch = useDispatch();
@@ -11,6 +17,7 @@ const ProductDetailsPage = () => {
     <ProductDetailsPageComponent
       addToCartReduxAction={addToCart}
       dispatch={dispatch}
+      getProductDetails={getProductDetails}
     />
   );
 };
