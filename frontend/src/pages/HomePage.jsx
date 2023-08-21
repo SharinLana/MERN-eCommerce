97b3a7d788232b3afc03e9ae25ae90874b7c0872
Lana Sharin <lana.sharin.webdev@gmail.com>
2023-08-21 16:47:53 -0700

    refactor the HomePage.jsx by moving its code to a separate component

--- frontend/src/pages/HomePage.jsx
+++ frontend/src/pages/HomePage.jsx
@@ -1,31 +1,8 @@
 import React from "react";
-import { Row, Container } from "react-bootstrap";
-import CarouselComponent from "../components/CarouselComponent";
-import CategoryCardComponent from "../components/CategoryCardComponent";
-
-const categories = [
-  "Audio",
-  "Movies",
-  "Books",
-  "Electronics",
-  "Cars",
-  "Smartphones",
-];
+import HomePageComponent from "./components/HomePageComponent";
 
 const HomePage = () => {
-  return (
-    <>
-      <CarouselComponent />
-
-      <Container style={{marginTop: "40px"}}>
-        <Row xs={1} md={2} className="g-4">
-          {categories.map((item, index) => (
-            <CategoryCardComponent item={item} key={index} />
-          ))}
-        </Row>
-      </Container>
-    </>
-  );
+  return <HomePageComponent />;
 };
 
 export default HomePage;
---
+++ frontend/src/pages/components/HomePageComponent.js
@@ -0,0 +1,31 @@
+import React from "react";
+import { Row, Container } from "react-bootstrap";
+import CarouselComponent from "../../components/CarouselComponent";
+import CategoryCardComponent from "../../components/CategoryCardComponent";
+
+const categories = [
+  "Audio",
+  "Movies",
+  "Books",
+  "Electronics",
+  "Cars",
+  "Smartphones",
+];
+
+const HomePageComponent = () => {
+  return (
+    <>
+      <CarouselComponent />
+
+      <Container style={{ marginTop: "40px" }}>
+        <Row xs={1} md={2} className="g-4">
+          {categories.map((item, index) => (
+            <CategoryCardComponent item={item} key={index} />
+          ))}
+        </Row>
+      </Container>
+    </>
+  );
+};
+
+export default HomePageComponent;
