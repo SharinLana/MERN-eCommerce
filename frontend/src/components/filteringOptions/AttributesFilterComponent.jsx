17dfcd75f7c4a37d6a5796994e1e2554d81e49ae
Lana Sharin <lana.sharin.webdev@gmail.com>
2023-08-22 14:26:24 -0700

    implement filters for attributes

--- frontend/src/components/filteringOptions/AttributesFilterComponent.jsx
+++ frontend/src/components/filteringOptions/AttributesFilterComponent.jsx
@@ -3,27 +3,65 @@ import React from "react";
 import { Form } from "react-bootstrap";
 
 const AttributesFilterComponent = ({ attrsFilter, setAttrsFromFilter }) => {
-
   return (
     <>
       {attrsFilter &&
         attrsFilter.length > 0 &&
-        attrsFilter.map((item, idx) => {
+        attrsFilter.map((filter, idx) => {
           return (
             <div key={idx}>
               <Form.Label>
-                <b>{item.key}</b>
+                <b>{filter.key}</b>
               </Form.Label>
-              {item.value.map((val, i) => {
+              {filter.value.map((valForKey, i) => {
                 return (
                   <Form.Check
                     key={i}
                     type="checkbox"
                     id="default-checkbox"
-                    label={val}
-                    onChange={e => setAttrsFromFilter(items => {
-                      console.log(item.key)
-                    })}
+                    label={valForKey}
+                    onChange={(e) =>
+                      setAttrsFromFilter((filters) => {
+                        if (filters.length === 0) {
+                          return [{ key: filter.key, values: [valForKey] }];
+                        }
+                        // If filters length is > than 0:
+                        let index = filters.findIndex(
+                          (item) => item.key === filter.key
+                        );
+
+                        if (index === -1) {
+                          return [
+                            ...filters,
+                            { key: filter.key, values: [valForKey] },
+                          ];
+                        }
+
+                        if (e.target.checked) {
+                          filters[index].values.push(valForKey);
+                          let unique = [...new Set(filters[index].values)];
+                          filters[index].values = unique;
+
+                          return [...filters]; // return modified filters
+                        }
+
+                        // If checked key was unchecked (user changed his mind and unchecked the box)
+                        let valuesWithoutUnchecked = filters[
+                          index
+                        ].values.filter((val) => val !== valForKey);
+
+                        filters[index].values = valuesWithoutUnchecked;
+                        if (valuesWithoutUnchecked.length > 0) {
+                          return [...filters];
+                        } else {
+                          let filtersWithoutOneKey = filters.filter(
+                            (item) => item.key !== filter.key
+                          );
+                          return [...filtersWithoutOneKey];
+                        }
+
+                      })
+                    }
                   />
                 );
               })}
